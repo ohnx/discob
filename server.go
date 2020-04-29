f641d4e23f3c04f0430e30102ac138381a2f93df
ohnx <me@masonx.ca>
2020-04-29 21:58:58 +0000

    Add content-type detection and 404 errors

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
     "strconv"
     "fmt"
     "strings"
+    "path/filepath"
     "flag"
     "html/template"
 )
@@ -19,12 +20,29 @@ type TemplateHelper struct {
     Files []GitFile
 }
 
-func GitRequestHandler(w http.ResponseWriter, r *http.Request) {
-    
-}
-
-func DefaultRequestHandler(w http.ResponseWriter, r *http.Request) {
-    
+func GuessMimeType(filename string, file string) string {
+    switch filepath.Ext(filename) {
+    case ".css": return "text/css"
+    case ".coffee": return "text/coffeescript"
+    case ".eot": return "application/vnd.ms-fontobject"
+    case ".htm": return "text/html"
+    case ".html": return "text/html"
+    case ".ics": return "text/calendar"
+    case ".js": return "application/javascript"
+    case ".json": return "application/json"
+    case ".markdown": return "text/markdown"
+    case ".md": return "text/markdown"
+    case ".otf": return "font/otf"
+    case ".pdf": return "application/pdf"
+    case ".svg": return "image/svg+xml"
+    case ".swf": return "application/x-shockwave-flash"
+    case ".wasm": return "application/wasm"
+    case ".woff": return "application/font-woff"
+    case ".woff2": return "application/font-woff2"
+    case ".xml": return "text/xml"
+    case ".yml": return "text/yaml"
+    default: return ""
+    }
 }
 
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,40 +51,57 @@ func RequestHandler(w http.ResponseWriter, r *http.Request) {
         revid, err := githelper.FetchRevision(domainParts[0])
 
         if err != nil {
-            fmt.Fprintf(w, "Commit not found: %s", err)
-            goto done
+            w.WriteHeader(http.StatusNotFound)
+            w.Header().Add("Content-Type", "text/plain")
+            fmt.Fprintf(w, "Commit not found: %s\n", err)
+            return
         }
 
         if r.URL.Path[len(r.URL.Path)-1:] == "/" {
             // ends with forward slash, so this is a directory
-            result, err := githelper.FetchTreeAtRevision(revid, r.URL.Path[1:len(r.URL.Path)-1])
+            dir := r.URL.Path[:len(r.URL.Path)-1]
+            if len(dir) > 0 {
+                dir = dir[1:]
+            }
+            result, err := githelper.FetchTreeAtRevision(revid, dir)
             if err != nil {
-                fmt.Fprintf(w, "File not found: %s", err)
-                goto done
+                w.WriteHeader(http.StatusNotFound)
+                w.Header().Add("Content-Type", "text/plain")
+                fmt.Fprintf(w, "Directory not found: %s\n", err)
+                return
             }
 
+            w.Header().Add("Content-Type", "text/html")
             err = lsttmpl.Execute(w, TemplateHelper{
                 Path: r.URL.Path,
                 Files: result,
             })
 
             if err != nil {
-                fmt.Fprintf(w, "Error executing template: %s", err)
+                fmt.Fprintf(w, "Error executing template: %s\n", err)
             }
         } else {
             result, err := githelper.FetchFileAtRevision(revid, r.URL.Path[1:])
             if err != nil {
-                fmt.Fprintf(w, "File not found: %s", err)
-                goto done
+                w.WriteHeader(http.StatusNotFound)
+                w.Header().Add("Content-Type", "text/plain")
+                fmt.Fprintf(w, "File not found: %s\n", err)
+                return
             }
-            fmt.Fprintf(w, "%s", result)
+
+            mimeGuess := GuessMimeType(r.URL.Path, result)
+            if len(mimeGuess) > 0 {
+                // only set it if the guess was successful, otherwise let
+                // the internal server deal with it
+                w.Header().Set("Content-Type", mimeGuess)
+            }
+            fmt.Fprint(w, result)
         }
     } else {
-        fmt.Fprintf(w, "I have no idea what to serve you")
+        w.WriteHeader(http.StatusNotFound)
+        w.Header().Add("Content-Type", "text/plain")
+        fmt.Fprintf(w, "I have no idea what to serve you\n")
     }
-
-done:
-    fmt.Fprintf(w, "\n")
 }
 
 func main() {
@@ -85,7 +120,7 @@ func main() {
 
     // Start server
     http.HandleFunc("/", RequestHandler)
-    log.Printf("Server listening on 0.0.0.0:%d", *port)
+    log.Printf("Server listening on %s:%d", *host, *port)
     err := http.ListenAndServe(*host + ":" + strconv.Itoa(*port), nil)
     if err != nil {
         log.Fatalf("Failed to listen: %s", err)
